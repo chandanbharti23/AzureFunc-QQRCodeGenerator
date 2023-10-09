760e6930451a8b5b1ef49f96b43b10c5a9762bdb
Chandan <chandan.bharti@microchannel.com.au>
2023-10-10 09:21:45 +1100

    Add project files.

---
+++ Function1.cs
@@ -0,0 +1,43 @@
+using System;
+using System.IO;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+using QRCoder;
+
+namespace AzureFunc_QQRCodeGenerator
+{
+    public static class MicrochannelQRCodeGenerator
+    {
+        [FunctionName("MicrochannelQRCodeGenerator")]
+        public static async Task<IActionResult> Run(
+            [HttpTrigger(AuthorizationLevel.Anonymous, "get", "post", Route = null)] HttpRequest req,
+            ILogger log)
+        {
+            string ReqData = req.Query["ReqData"];
+
+            string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
+            dynamic data = JsonConvert.DeserializeObject(requestBody);
+            ReqData = ReqData ?? data?.url;
+            if (string.IsNullOrEmpty(ReqData))
+            {
+                return new BadRequestResult();
+            }
+
+            var generator = ReqData;
+            var payload = generator.ToString();
+
+            using (var qrGenerator = new QRCodeGenerator())
+            {
+                var qrCodeData = qrGenerator.CreateQrCode(payload, QRCodeGenerator.ECCLevel.Q);
+                var qrCode = new PngByteQRCode(qrCodeData);
+                var qrCodeAsPng = qrCode.GetGraphic(20);
+                return new FileContentResult(qrCodeAsPng, "image/png");
+            }
+        }
+    }
+}
